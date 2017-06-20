d615b6219c01b53bfffb69817832adadb7792574
Claudia413 <cmjengelsman@gmail.com>
2017-06-20 11:15:06 +0200

    Add game model

---
+++ src/models/games.model.js
@@ -0,0 +1,28 @@
+// games-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+
+  const player = new Schema ({
+    character: { type: String },
+    userId: { type: String},
+    clickCount: { type: Number}
+  });
+
+  const games = new Schema({
+    players: [playerSchema],
+    countdownTimer: { type: Number, default: 15},
+    start: { type: Boolean, default: false},
+    evenScore: { type: Boolean, default: true},
+    winner: { type: Boolean, default: false},
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+  });
+
+
+
+  return mongooseClient.model('games', games);
+};
